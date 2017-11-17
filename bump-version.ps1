5fc59b52477a20657f2f5a5b17682c90dde462ce
Mario Pietsch <pmariojo@gmail.com>
2017-11-17 17:04:56 +0100

    is deprecated, because doesn't work in unix systems. but keep it as reference

--- bump-version.ps1
+++ bump-version.ps1
@@ -1,3 +1,5 @@
+## just for testing. Don't use it
+
 $currentDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $output = . "GitVersion"
@@ -7,6 +9,6 @@ $versionInfo = $joined | ConvertFrom-Json
 $manifest = (Get-Content "$currentDir\addon\manifest.JSON)" -join "`n" | ConvertFrom-Json
 $manifest.version = $versionInfo.SemVer
 
-mkdir -f "$currentDir\Artifacts"
+mkdir -f "$currentDir\test"
 
-$manifest | ConvertTo-Json | Out-File -encoding ASCII "$currentDir\Artifacts\manifest.json"
+$manifest | ConvertTo-Json | Out-File -encoding ASCII "$currentDir\test\manifest.json"
